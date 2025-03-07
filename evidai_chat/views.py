af3bba36385763b2f6b0710fd4867d499f3f7ecd
hemali-dodia-evident <hemali@evident.capital>
2025-03-07 17:08:15 +0530

    remove italic

--- evidai_chat/views.py
+++ evidai_chat/views.py
@@ -1153,7 +1153,7 @@ def format_response(response):
 
     # Convert Markdown to HTML
     html_content = markdown.markdown(response)
-
+    html_content = html_content.replace("*","").replace("<em>","").replace("</em>","")
     return html_content
 
 # Main flow
