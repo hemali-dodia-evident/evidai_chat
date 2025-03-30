a261ffb90572d7c9cf0d61f3ef653a0996d9b519
hemali-dodia-evident <hemali@evident.capital>
2025-03-30 16:56:07 +0530

    no_heder_found_DB

--- evidai/middleware.py
+++ evidai/middleware.py
@@ -9,38 +9,55 @@ logger = logging.getLogger(__name__)
 class DatabaseSelectionMiddleware(MiddlewareMixin):
     def process_request(self, request):
         """Switch database based on request headers"""
-        host = request.headers.get('X-Environment').lower()
-        logging.info(f"{host}")
-        if 'prod' in host:
-            logging.info(f"Prod DB sesstings\n{os.getenv('DB_NAME')}\n{os.getenv('URL')}")
+        try:
+            host = request.headers.get('X-Environment').lower()
+            logging.info(f"{host}")
+            if 'prod' in host:
+                logging.info(f"Prod DB sesstings\n{os.getenv('DB_NAME')}\n{os.getenv('URL')}")
+                settings.DATABASES['default'] = {
+                            'ENGINE': 'django.db.backends.postgresql',
+                            'NAME': os.getenv('DB_NAME'),
+                            'USER': os.getenv('DB_USER'),
+                            'PASSWORD': os.getenv('DB_PASS'),
+                            'HOST': os.getenv('DB_HOST'),
+                            'PORT': os.getenv('DB_PORT'),
+                            'ATOMIC_REQUESTS': True, 
+                            'AUTOCOMMIT': True,
+                            'TIME_ZONE': 'UTC',
+                            'CONN_HEALTH_CHECKS': True,
+                            'CONN_MAX_AGE': 60,  # Keeps database connections open for reuse (in seconds)
+                            'OPTIONS': {},  # Additional database options (optional, can be empty)
+                        }
+                settings.URL=os.getenv('URL')
+            else:
+                logging.info(f"UAT DB sesstings\n{os.getenv('UAT_DB_NAME')}\n{os.getenv('UAT_URL')}")
+                settings.DATABASES['default'] = {
+                            'ENGINE': 'django.db.backends.postgresql',
+                            'NAME': os.getenv('UAT_DB_NAME'),
+                            'USER': os.getenv('UAT_DB_USER'),
+                            'PASSWORD': os.getenv('UAT_DB_PASS'),
+                            'HOST': os.getenv('UAT_DB_HOST'),
+                            'PORT': os.getenv('UAT_DB_PORT'),
+                            'ATOMIC_REQUESTS': True, 
+                            'TIME_ZONE': 'UTC',
+                            'CONN_HEALTH_CHECKS': True,
+                            'CONN_MAX_AGE': 60,  # Keeps database connections open for reuse (in seconds)
+                            'OPTIONS': {},  # Additional database options (optional, can be empty)
+                        }
+                settings.URL=os.getenv('UAT_URL')
+        except:
+            logging.info(f"Except UAT DB sesstings\n{os.getenv('UAT_DB_NAME')}\n{os.getenv('UAT_URL')}")
             settings.DATABASES['default'] = {
-                        'ENGINE': 'django.db.backends.postgresql',
-                        'NAME': os.getenv('DB_NAME'),
-                        'USER': os.getenv('DB_USER'),
-                        'PASSWORD': os.getenv('DB_PASS'),
-                        'HOST': os.getenv('DB_HOST'),
-                        'PORT': os.getenv('DB_PORT'),
-                        'ATOMIC_REQUESTS': True, 
-                        'AUTOCOMMIT': True,
-                        'TIME_ZONE': 'UTC',
-                        'CONN_HEALTH_CHECKS': True,
-                        'CONN_MAX_AGE': 60,  # Keeps database connections open for reuse (in seconds)
-                        'OPTIONS': {},  # Additional database options (optional, can be empty)
-                    }
-            settings.URL=os.getenv('URL')
-        else:
-            logging.info(f"UAT DB sesstings\n{os.getenv('UAT_DB_NAME')}\n{os.getenv('UAT_URL')}")
-            settings.DATABASES['default'] = {
-                        'ENGINE': 'django.db.backends.postgresql',
-                        'NAME': os.getenv('UAT_DB_NAME'),
-                        'USER': os.getenv('UAT_DB_USER'),
-                        'PASSWORD': os.getenv('UAT_DB_PASS'),
-                        'HOST': os.getenv('UAT_DB_HOST'),
-                        'PORT': os.getenv('UAT_DB_PORT'),
-                        'ATOMIC_REQUESTS': True, 
-                        'TIME_ZONE': 'UTC',
-                        'CONN_HEALTH_CHECKS': True,
-                        'CONN_MAX_AGE': 60,  # Keeps database connections open for reuse (in seconds)
-                        'OPTIONS': {},  # Additional database options (optional, can be empty)
-                    }
+                            'ENGINE': 'django.db.backends.postgresql',
+                            'NAME': os.getenv('UAT_DB_NAME'),
+                            'USER': os.getenv('UAT_DB_USER'),
+                            'PASSWORD': os.getenv('UAT_DB_PASS'),
+                            'HOST': os.getenv('UAT_DB_HOST'),
+                            'PORT': os.getenv('UAT_DB_PORT'),
+                            'ATOMIC_REQUESTS': True, 
+                            'TIME_ZONE': 'UTC',
+                            'CONN_HEALTH_CHECKS': True,
+                            'CONN_MAX_AGE': 60,  # Keeps database connections open for reuse (in seconds)
+                            'OPTIONS': {},  # Additional database options (optional, can be empty)
+                        }
             settings.URL=os.getenv('UAT_URL')
